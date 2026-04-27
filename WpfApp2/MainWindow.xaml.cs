971550d5fbd67284a644f80dc78d630f04f55dd7
Nazhik1 <novozhilov_dp@mail.ru>
2026-04-27 22:22:01 +0300

    lab2

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
 
 namespace WpfApp2
 {
@@ -24,10 +26,52 @@ namespace WpfApp2
         public void got(object sender, RoutedEventArgs e)
         {
             TextBox tb = sender as TextBox;
-            if (tb.Text == "0" || tb.Text == "Введите пример:")
+            if (tb.Text == "0" || tb.Text == "Введите пример:" || tb.Text == "Ошибка: деление на 0" || tb.Text == "Ошибка в примере")
             {
                 tb.Text = "";
             }
         }
+
+        public void ClickButton(object sender, RoutedEventArgs e)
+        {
+            Number.Focus();
+            Button btn = sender as Button;
+            string btn_content = btn.Content.ToString();
+            switch (btn_content)
+            {
+                case "1": Number.Text += btn_content; break;
+                case "2": Number.Text += btn_content; break;
+                case "3": Number.Text += btn_content; break;
+                case "4": Number.Text += btn_content; break;
+                case "5": Number.Text += btn_content; break;
+                case "6": Number.Text += btn_content; break;
+                case "7": Number.Text += btn_content; break;
+                case "9": Number.Text += btn_content; break;
+                case "8": Number.Text += btn_content; break;
+                case "0": Number.Text += btn_content; break;
+                case "C": Number.Text = ""; break;
+                case "+": Number.Text += btn_content; break;
+                case "-": Number.Text += btn_content; break;
+                case "*": Number.Text += btn_content; break;
+                case "/": Number.Text += btn_content; break;
+                case "=":
+                    try
+                    {
+                        var result = new DataTable().Compute(Number.Text, null);
+                        double Dres = Convert.ToDouble(result);
+                        if (double.IsInfinity(Dres))
+                        {
+                            Number.Text = "Ошибка: деление на 0";
+                        }
+                        else { Number.Text = Dres.ToString(); }
+                    }
+                    catch (Exception) {
+                        Number.Text = "Ошибка в примере";
+                    }
+                        break;
+
+            }
+        }
     }
+    
 }
