f4d4a4a05c6fadc69651ad14eae6d8f6d61453ff
Nazhik1 <novozhilov_dp@mail.ru>
2026-05-01 03:33:47 +0300

    lab3 add

--- WpfApp3/MainWindow.xaml.cs
+++ WpfApp3/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +10,10 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp3
 {
@@ -20,18 +22,39 @@ namespace WpfApp3
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private string currentFilePath;
+        private string filesFolder;
         public MainWindow()
         {
             InitializeComponent();
-        }
-        public void Add(object sender, RoutedEventArgs e)
-        {
-            Zam.Items.Add("Заметка");
+            string baseDirectory = @"C:\Users\Dmitriy\AppData\Local\lab3";
+
+            filesFolder = System.IO.Path.Combine(baseDirectory, "Data");
+            LoadFile();
         }
 
         public void Del(object sender, RoutedEventArgs e)
         {
             Zam.Items.Remove(Zam.SelectedItem);
+        }
+        public void LoadFile()
+        {
+            Zam.Items.Clear();
+            string[] txtFiles = Directory.GetFiles(filesFolder, "*.txt");
+ 
+
+            foreach (string file in txtFiles)
+            {
+                Zam.Items.Add(System.IO.Path.GetFileName(file));
+            }
+        }
+        private void Add(object sender, RoutedEventArgs e)
+        {
+            string fileName = "отчёт.txt";
+            string fullPath = Path.Combine(filesFolder, fileName);
+
+            File.WriteAllText(fullPath, "");
         }
     }
 }
